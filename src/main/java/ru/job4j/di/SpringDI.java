ba40a1d517292609b096b2e654ba9fb224d855a6
OtBbNA <tosha.kostylev.97@mail.ru>
2024-11-19 23:28:10 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -9,10 +9,17 @@ public class SpringDI {
         context.refresh();
 
         StartUI ui = context.getBean(StartUI.class);
-
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
         ui.input("Select some of:");
+
+        Store store = context.getBean(Store.class);
+        store.add("1");
+
+        Store another = context.getBean(Store.class);
+        another.add("2");
+
+        another.getAll().forEach(System.out::println);
     }
 }
