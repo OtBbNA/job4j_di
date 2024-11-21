304c8aa79601da6add77e1845e38b76ce76f85a0
OtBbNA <tosha.kostylev.97@mail.ru>
2024-11-21 16:44:09 +0300

    2. Сканирование проекта [#1070 #522720]

--- src/main/java/ru/job4j/lifecycle/model/Product.java
+++ src/main/java/ru/job4j/lifecycle/model/Product.java
@@ -18,7 +18,7 @@ public class Product implements BeanNameAware, BeanFactoryAware, ApplicationCont
     private BeanFactory beanFactory;
 
     public Product(String name, double price) {
-        System.out.printf("1. ������ ����������� %s%n", getClass().getSimpleName());
+        System.out.printf("1. вызван конструктор %s%n", getClass().getSimpleName());
         this.name = name;
         this.price = price;
     }
@@ -26,47 +26,47 @@ public class Product implements BeanNameAware, BeanFactoryAware, ApplicationCont
     @Override
     public void setBeanName(String name) {
         beanName = name;
-        System.out.printf("2. ���� ��� ��������������� � ��������� ��� ������ '%s'%n", name);
+        System.out.printf("2. этот бин зарегистрирован в контексте под именем '%s'%n", name);
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         this.beanFactory = beanFactory;
         boolean result = this.beanFactory.containsBean(beanName);
-        System.out.printf("2. ��������� BeanFactory � ��� '%s', ��������� ���������: '%s'%n", beanName, result);
+        System.out.printf("2. установка BeanFactory в бин '%s', Результат установки: '%s'%n", beanName, result);
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.printf("2. ����������� �������� setApplicationContext ���� '%s'%n", beanName);
+        System.out.printf("2. выполняются действия setApplicationContext бина '%s'%n", beanName);
     }
 
     @PostConstruct
     public void annotInitMethod() {
-        System.out.printf("4. @PostConstruct ����������� �������� ��� ������������� ���� '%s'%n", beanName);
+        System.out.printf("4. @PostConstruct выполняются действия при инициализация бина '%s'%n", beanName);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.printf("4. InitializingBean: ����������� �������� ��� ������������� ���� '%s'%n", beanName);
+        System.out.printf("4. InitializingBean: выполняются действия при инициализация бина '%s'%n", beanName);
     }
 
     public void initMethod() {
-        System.out.printf("4. @Bean, �������� initMethod: ����������� �������� ��� ������������� ���� '%s'%n", beanName);
+        System.out.printf("4. @Bean, свойство initMethod: выполняются действия при инициализация бина '%s'%n", beanName);
     }
 
     @PreDestroy
     public void oneMethodDestroy() {
-        System.out.printf("6. @PreDestroy: ����������� �������� ��� ����������� ���� '%s'%n", beanName);
+        System.out.printf("6. @PreDestroy: выполняются действия при уничтожении бина '%s'%n", beanName);
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.printf("6. DisposableBean: ����������� �������� ��� ����������� ���� '%s'%n", beanName);
+        System.out.printf("6. DisposableBean: выполняются действия при уничтожении бина '%s'%n", beanName);
     }
 
     public void destroyMethod() {
-        System.out.printf("6. @Bean, �������� destroyMethod: - ����������� �������� ��� ����������� ���� '%s'%n", beanName);
+        System.out.printf("6. @Bean, свойство destroyMethod: - выполняются действия при уничтожении бина '%s'%n", beanName);
     }
 
     public String getName() {
